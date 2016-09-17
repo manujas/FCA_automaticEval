9bd01ec66e187fe1436b89a5c0bb7c5411f31d33
manujas <arias.emanuel@gmail.com>
2016-09-17 11:17:15 -0300

    primera vistas

--- ckan_consumer/views.py
+++ ckan_consumer/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'ckan_consumer/index.html')
+    query = None
+    if request.method == 'GET' and 'q' in request.GET:
+        query = request.GET['q']
+
+    return render(request, 'ckan_consumer/index.html', {'query': query})
