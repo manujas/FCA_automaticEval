6de647c5ca4724827ddb0d7a0bc6a8b7c99d8b77
manujas <arias.emanuel@gmail.com>
2016-09-19 19:09:49 -0300

    se agrega manejo de excepciones básico y mensajes flash

--- web/views.py
+++ web/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
+from django.contrib import messages
 from ckan_consumer import ckan_search
 
 def index(request):
     data = None
+    query = None
+
     if request.method == 'GET' and 'q' in request.GET:
         query = request.GET['q']
 
         url = 'http://www.datos.gob.ar/'
-        data = ckan_search.package_search(url, query, 10)
 
-    return render(request, 'web/index.html', {'data': data})
+        try:
+            data = ckan_search.package_search(url, query, 10)
+        except Exception as e:
+            messages.error(request, 'Algo sucedio mal, vuelve a intentarlo.')
+
+        return render(request, 'web/index.html', {'data': data, 'query': query})
+
+    return render(request, 'web/index.html')
